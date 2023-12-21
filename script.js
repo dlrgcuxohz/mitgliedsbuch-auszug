53b36d819bd454304ea05e8b13fa90b0be819562
Tjark Saul <tjark.saul@bez-cux-ohz.dlrg.de>
2023-12-21 18:39:17 +0100

    Link [WIP]

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ const uploadField = document.getElementById("upload");
 const dropArea = document.getElementById("img-container");
 const img = document.getElementById("target");
 const body = document.getElementsByTagName("body")[0];
+const urlOutput = document.getElementById('url');
+
+const fields = [
+  "name",
+  "vorname",
+  "geb",
+  "str",
+  "plz",
+  "ort",
+  "eintritt",
+  "abgelegt",
+  "kombi",
+  "selbsterklaerung",
+  "lv", 
+  "bezirk", 
+  "gliederung", 
+  "wettkampfjahr",
+  "pruefungen",
+  "tauglichkeit",
+];
 
 function handleFileSelect(evt) {
   let files = evt.target.files;
@@ -66,21 +86,26 @@ function deletePersonalData() {
     "selbsterklaerung",
   ].forEach((item) => {
     document.getElementById(item).value = "";
+    values[item] = "";
   });
   ["pruefungen", "tauglichkeit"].forEach((name) => {
     const elements = document.getElementsByName(name);
     elements.forEach((item) => {
       item.checked = false;
     });
-    img.src = "";
-    dropArea.classList.remove("has-image");
+    values[name] = "";
   });
+  img.src = "";
+  dropArea.classList.remove("has-image");
+  dumpValues();
 }
 
 function deleteNonPersonalData() {
   ["lv", "bezirk", "gliederung", "wettkampfjahr"].forEach((item) => {
     document.getElementById(item).value = "";
+    values[item] = "";
   });
+  dumpValues();
 }
 
 function prepareFileUpload() {
@@ -124,4 +149,120 @@ function prepareFileUpload() {
   });
 }
 
+function fallbackCopyTextToClipboard(text) {
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+  
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    document.execCommand('copy');
+  } catch (err) {
+    document.body.removeChild(textArea);
+    throw err;
+  }
+
+  document.body.removeChild(textArea);
+}
+
+async function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+    await navigator.clipboard.writeText(text)
+}
+
+function parseUrl(str) {
+  const url = new URL(str);
+  const hash = url.hash;
+  const vals = hash.split('#')[1].split('&').map(e => e.split('=')).reduce((acc, cur) => {
+    if (!fields.includes(cur[0])) { return acc; }
+    acc[cur[0]] = decodeURIComponent(cur[1]);
+    return acc;
+  }, {});
+  values = vals;
+
+  Object.keys(values).forEach(cur => {
+    const el = document.getElementById(cur);
+    if (el) {
+      if (el.type === "radio") {
+        el.checked = values[cur] === "on";
+      } else {
+        el.value = values[cur];
+      }
+      return;
+    }
+    const els = document.getElementsByName(cur);
+    els.forEach(el => {
+      if (el.type === "radio" && el.id == values[cur]) {
+        el.checked = true;
+      } else if (el.type === "radio") {
+        el.checked = false;
+      }
+    });
+  });
+  dumpValues();
+}
+
+let values = {};
+
+function dumpValues() {
+  const components = Object.keys(values).reduce((acc, cur) => values[cur].trim() !== "" ? `${acc}&${cur}=${encodeURIComponent(values[cur].trim())}` : acc, '').replace(/^&/g, '');
+  const url = new URL(window.location.href);
+  url.hash = `#${components}`;
+  urlOutput.value = url;
+  if (url.href !== window.location.href) {
+    window.location.href = url;
+  }
+}
+
+function prepareUrlSupport() {
+  urlOutput.addEventListener("focus", () => {
+    urlOutput.select();
+  });
+
+  const inputs = [...document.getElementsByTagName("input"), ...document.getElementsByTagName("select")];
+  inputs.forEach(input => {
+    if (input.id == 'url') {
+      return;
+    }
+    input.addEventListener("change", e => {
+      if (input.type === "radio") {
+        values[input.name] = input.id;
+      }  else {
+        values[input.id] = e.target.value;
+      }
+      dumpValues();
+    })
+  })
+
+  const copyButton = document.getElementById('copy-button');
+  copyButton.addEventListener("click", async e => {
+    e.preventDefault();
+    try {
+      await copyTextToClipboard(urlOutput.value);
+      copyButton.classList.add("copied");
+      window.setTimeout(() => copyButton.classList.remove("copied"), 10000);
+    } catch (err) {
+      console.error('Could not copy text: ', err);
+    }
+  });
+
+  window.addEventListener("hashchange", event => {
+    const url = event.newURL;
+    parseUrl(url);
+  });
+
+  parseUrl(window.location.href);
+}
+
 prepareFileUpload();
+prepareUrlSupport();
